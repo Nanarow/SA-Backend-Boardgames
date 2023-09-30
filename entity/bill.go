35f158ed665da41017f2a3ac53eff8f7feafe82b
woranan sholee <woranan.sholee@gmail.com>
2023-09-30 17:18:21 +0700

    embedded

--- entity/bill.go
+++ entity/bill.go
@@ -1,14 +1,16 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Bill struct {
 	gorm.Model
 	MemberID uint
 	Status   string
 	Total    float64
-
-	// RoomBills   []RoomBill   `gorm:"foreignKey:ID"`
-	GameBills []GameBill `gorm:"foreignKey:ID"`
-	// MemberBills []MemberBill `gorm:"foreignKey:ID"`
+	Slip     string
+	PayDate  time.Time
 }
