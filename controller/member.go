c175447e17d694d9375e0d3bc376ab54dc2cc78b
woranan sholee <woranan.sholee@gmail.com>
2023-10-09 01:52:03 +0700

    update

--- controller/member.go
+++ controller/member.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -15,33 +14,32 @@ func GetMemberById(c *gin.Context) {
 	id := c.Param("id")
 	err := entity.DB().First(&member, id).Error
 	if !isError(err, c) {
-		err = entity.DB().Where("member_id = ?", id).Order("pay_date desc").First(&memberBill).Error
-		if err != nil {
-			if member.MemberTypeID != 5 {
+		if member.MemberTypeID != 5 {
+			err = entity.DB().Where("member_id = ?", id).Order("pay_date desc").First(&memberBill).Error
+			if err != nil {
 				member.MemberTypeID = 1
 				entity.DB().Save(&member)
+			} else {
+				// timeOut := time.Since(memberBill.PayDate).Hours() / 24.00 // for 30 days
+				// if timeOut > 30 {
+				// 	if member.MemberTypeID != 5 {
+				// 		member.MemberTypeID = 1
+				// 		entity.DB().Save(&member)
+				// 	}
+				// }
+				timeOut := time.Since(memberBill.PayDate).Minutes() // for 2 minutes
+				if timeOut > 2 {
+					member.MemberTypeID = 1
+					entity.DB().Save(&member)
+				}
 			}
-		}
-		// timeOut := time.Since(memberBill.PayDate).Hours() / 24.00 // 30 days
-		// if timeOut > 30 {
-		// 	if member.MemberTypeID != 5 {
-		// 		member.MemberTypeID = 1
-		// 		entity.DB().Save(&member)
-		// 	}
-		// }
-		timeOut := time.Since(memberBill.PayDate).Minutes() // 30 days
-		fmt.Println("down :", timeOut)
-		if timeOut > 2 {
-			if member.MemberTypeID != 5 {
-				member.MemberTypeID = 1
-				entity.DB().Save(&member)
+			err = entity.DB().Preload("MemberType").First(&member, id).Error
+			if isError(err, c) {
+				return
 			}
+			c.JSON(http.StatusOK, gin.H{"data": member})
+
 		}
-		err = entity.DB().Preload("MemberType").First(&member, id).Error
-		if isError(err, c) {
-			return
-		}
-		c.JSON(http.StatusOK, gin.H{"data": member})
 	}
 
 }
